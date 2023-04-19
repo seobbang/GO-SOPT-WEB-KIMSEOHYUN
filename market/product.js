b3a30ce99b4ceea7ba5f110ca471c5697783ad82
seobbang <seobbangway@naver.com>
2023-04-19 22:55:15 +0900

    style: 구조분해할당으로 변경, 데이터 변수 이름 변경

--- market/product.js
+++ market/product.js
@@ -3,68 +3,68 @@ const $$ = (selector) => document.querySelectorAll(selector);
 
 const PRODUCT_DATA = [
   {
-    kind: "ramyun",
+    category: "ramyun",
     name: "불닭볶음면",
     tag: ["매운맛", "코스빌지수"],
     img: "images/불닭볶음면.jpg",
   },
   {
-    kind: "snack",
+    category: "snack",
     name: "무뚝뚝 감자칩",
     tag: ["후추", "감자칩", "양념감자"],
     img: "images/무뚝뚝감자칩.webp",
   },
   {
-    kind: "snack",
+    category: "snack",
     name: "프링글스 오리지널",
     tag: ["오리지널", "미국"],
     img: "images/프링글스.jpg",
   },
   {
-    kind: "ramyun",
+    category: "ramyun",
     name: "왕뚜껑",
     tag: ["컵라면", "킹뚜껑 도전"],
     img: "images/왕뚜껑.jpg",
   },
   {
-    kind: "drink",
+    category: "drink",
     name: "제로 닥터페퍼",
     tag: ["무설탕", "탄산", "미국 1위"],
     img: "images/닥터페퍼.webp",
   },
   {
-    kind: "drink",
+    category: "drink",
     name: "코카콜라",
     tag: ["탄산", "근본"],
     img: "images/코카콜라.jpg",
   },
   {
-    kind: "ramyun",
+    category: "ramyun",
     name: "신라면",
     tag: ["내 최애 라면", "매움"],
     img: "images/신라면.jpg",
   },
   {
-    kind: "drink",
+    category: "drink",
     name: "포카리스웨트",
     tag: ["이온 음료", "청량함", "시원함"],
     img: "images/포카리스웨트.jpg",
   },
   {
-    kind: "ramyun",
+    category: "ramyun",
     name: "진라면 매운맛",
     tag: ["이온 음료", "청량함", "시원함"],
     img: "images/진라면.png",
   },
   {
-    kind: "drink",
+    category: "drink",
     name: "옥수수수염차",
     tag: ["0칼로리", "심지어 맛있음"],
     img: "images/옥수수.jpg",
   },
 
   {
-    kind: "snack",
+    category: "snack",
     name: "홈런볼",
     tag: ["얼려도", "맛있어", "시원함"],
     img: "images/홈런볼.jpg",
@@ -73,31 +73,30 @@ const PRODUCT_DATA = [
 
 const cardSection = $(".card__section");
 
-PRODUCT_DATA.forEach((item) => {
+PRODUCT_DATA.forEach(({ category, name, tag, img }) => {
   let tagList = "";
-  const { tag } = item;
   tag.forEach((tag) => {
     tagList += `<li>${tag}</li>`;
   });
 
-  cardSection.innerHTML += `<article class="${item.kind}" id="${item.name
+  cardSection.innerHTML += `<article class="${category}" id="${name
     .split(" ")
     .join("")}">
-            <header>${item.name}</header>
+            <header>${name}</header>
             <div class="tagContainer">
                 <ul>${tagList}</ul>
-                <button type="button" class="showMore ${item.name
+                <button type="button" class="showMore $item.name
                   .split(" ")
                   .join("")}">+</button>
             </div>
             <div class="tagModal hidden">
                 <ul>${tagList}
-                <button type="button" class="close ${item.name
+                <button type="button" class="close ${name
                   .split(" ")
                   .join("")}">x</button></ul>
             </div>
-            <img src="${item.img}" alt="상품 사진" />
-            <button type="button"  class="heartButton">
+            <img src="${img}" alt="상품 사진"  />
+            <button type="button" class="heartButton" onclick="javascript:handle()">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 height="48"
