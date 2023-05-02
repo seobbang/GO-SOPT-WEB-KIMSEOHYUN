feaa5d5e97137b2cde96aef992d1a293e9de09d9
seobbang <seobbangway@naver.com>
2023-05-03 02:02:17 +0900

    feat: 카드 쌍을 맞춘 경우 깜빡이는 애니메이션 적용

--- find-ompangi/src/components/Header.jsx
+++ find-ompangi/src/components/Header.jsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Header = ({ level, score }) => {
   const cardCount = level === "EASY" ? 5 : level === "NORMAL" ? 7 : 9;
+  const [isBlink, setIsBlink] = useState(false);
+
+  useEffect(() => {
+    if (score === 0) return;
+    setIsBlink(true);
+    setTimeout(() => {
+      setIsBlink(false);
+    }, 2500);
+  }, [score]);
 
   return (
     <St.HeaderContainer>
       <h1>🔮 옴팡이를 맞춰주세요 🔮</h1>
-      <St.Score>
+      <St.Score className={isBlink && "blink"}>
         {score} / {cardCount}
       </St.Score>
     </St.HeaderContainer>
@@ -24,6 +34,7 @@ const St = {
     align-items: center;
     justify-content: center;
 
+    width: 100%;
     height: 15rem;
 
     font-size: 3.5em;
@@ -33,5 +44,16 @@ const St = {
   `,
   Score: styled.h2`
     margin-top: 2rem;
+
+    @keyframes blink-effect {
+      50% {
+        opacity: 0;
+      }
+    }
+
+    &.blink {
+      animation: blink-effect 0.8s step-end infinite;
+      font-weight: bold;
+    }
   `,
 };
