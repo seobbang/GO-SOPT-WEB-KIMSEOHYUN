10d4b52525a4b394749c99b1ad820074f14f3e18
seobbang <seobbangway@naver.com>
2023-05-09 17:27:51 +0900

    feat: 중복되는 할 일 추가 막기 구현

--- ToDoMate/todolist.js
+++ ToDoMate/todolist.js
@@ -93,24 +93,38 @@ function handleClickToDoList(e) {
 /** 할 일 추가 함수 **/
 function handleSubmitButton(category) {
   const ul = $(`ul.${category}`);
+  const li = $$(`ul.${category} > li`);
 
-  ul.innerHTML += `
-  <li>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      height="48"
-      viewBox="0 96 960 960"
-      width="48"
-    >
-      <path
-        d="m480 935-41-37q-106-97-175-167.5t-110-126Q113 549 96.5 504T80 413q0-90 60.5-150.5T290 202q57 0 105.5 27t84.5 78q42-54 89-79.5T670 202q89 0 149.5 60.5T880 413q0 46-16.5 91T806 604.5q-41 55.5-110 126T521 898l-41 37Z"
-      />
-    </svg>
-  ${input.value}
-  </li>`;
+  let isDuplicate = false;
 
-  input.value = ""; // input 비우기
-  todayCount.innerText++; // 할 일 개수 1 추가
+  li.forEach(({ innerText }) => {
+    innerText.replace(/\n/g, "");
+    innerText.replaceAll(" ", "");
+    if (input.value === innerText) {
+      isDuplicate = true;
+    }
+  });
+
+  if (isDuplicate) {
+    alert("이미 있는 항목입니다.");
+  } else {
+    ul.innerHTML += `
+    <li>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="48"
+        viewBox="0 96 960 960"
+        width="48"
+      >
+        <path
+          d="m480 935-41-37q-106-97-175-167.5t-110-126Q113 549 96.5 504T80 413q0-90 60.5-150.5T290 202q57 0 105.5 27t84.5 78q42-54 89-79.5T670 202q89 0 149.5 60.5T880 413q0 46-16.5 91T806 604.5q-41 55.5-110 126T521 898l-41 37Z"
+        />
+      </svg>
+    ${input.value}
+    </li>`;
+    input.value = ""; // input 비우기
+    todayCount.innerText++; // 할 일 개수 1 추가
+  }
 }
 
 toDoListSection.addEventListener("click", handleClickToDoList);
